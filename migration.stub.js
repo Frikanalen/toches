f4b86a190c37c908628fec00d8314540c7375cfd
Enitoni <enitoni@gmail.com>
2021-11-10 19:03:02 +0100

    Fix stub types

--- migration.stub.js
+++ migration.stub.js
@@ -1,11 +1,11 @@
 //@ts-check
 
 /**
- * @param {import("knex")} knex
+ * @param {import("knex").Knex} knex
  */
 exports.up = async (knex) => {}
 
 /**
- * @param {import("knex")} knex
+ * @param {import("knex").Knex} knex
  */
 exports.down = async (knex) => {}
