c797e726292c3fbe9f66940dc664f693396c0e21
Enitoni <enitoni@gmail.com>
2021-11-11 00:41:04 +0100

    Fix session being secure on dev

--- src/modules/auth/middleware/useSession.ts
+++ src/modules/auth/middleware/useSession.ts
@@ -1,6 +1,7 @@
 import session from "koa-session"
 import Koa from "koa"
 import { db } from "../../db/db"
+import { IS_DEV, IS_PROD } from "../../core/constants"
 
 export const setCookie = async (key: string, session: any, maxAge: number) => {
   await deleteCookie(key)
@@ -30,11 +31,11 @@ export const useSession = (app: Koa) =>
         set: setCookie,
         destroy: deleteCookie,
       },
-      key: "skinship",
+      key: "fk:session",
       maxAge: 604800000, // a week
       httpOnly: true,
       signed: false,
-      secure: true,
+      secure: IS_PROD,
     },
     app,
   )
