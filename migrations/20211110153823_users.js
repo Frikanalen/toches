631b457eb56a694a24aee40642fdf56e318af422
Enitoni <enitoni@gmail.com>
2021-11-10 19:46:15 +0100

    add missing notNullable on user migration

--- migrations/20211110153823_users.js
+++ migrations/20211110153823_users.js
@@ -7,11 +7,11 @@ exports.up = async (knex) => {
   await knex.schema.createTable("users", (table) => {
     table.increments("id")
 
-    table.string("email").unique()
+    table.string("email").unique().notNullable()
     table.string("password").notNullable()
 
-    table.string("first_name")
-    table.string("last_name")
+    table.string("first_name").notNullable()
+    table.string("last_name").notNullable()
 
     table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable()
     table.timestamp("updated_at").defaultTo(knex.fn.now()).notNullable()
