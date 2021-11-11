cb17bf12fb854baf6fdd803d5c706fb5a1c8d822
Enitoni <enitoni@gmail.com>
2021-11-11 16:57:13 +0100

    move bcrypt cost factor into own constant

---
+++ src/modules/auth/constants.ts
@@ -0,0 +1,2 @@
+// This should be increased when CPUs catch up
+export const BCRYPT_COST_FACTOR = 12
--- src/modules/user/helpers/createUser.ts
+++ src/modules/user/helpers/createUser.ts
@@ -3,12 +3,11 @@ import bcrypt from "bcrypt"
 import { db } from "../../db/db"
 import { userModel } from "../models/userModel"
 import { getUser } from "./getUser"
-
-const COST_FACTOR = 12
+import { BCRYPT_COST_FACTOR } from "../../auth/constants"
 
 export const createUser = async (data: ValidatedUser) => {
   const { password, email, firstName, lastName } = data
-  const hashed = await bcrypt.hash(password, COST_FACTOR)
+  const hashed = await bcrypt.hash(password, BCRYPT_COST_FACTOR)
 
   const [id] = await db
     .insert({
