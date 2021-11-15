120d1b1546f101a7e1484935fdcd482bdc3dc2dd
Enitoni <enitoni@gmail.com>
2021-11-15 16:28:50 +0100

    expose email in serializeUser

--- src/modules/user/helpers/serializeUser.ts
+++ src/modules/user/helpers/serializeUser.ts
@@ -2,8 +2,8 @@ import { UserData } from "../models/userModel"
 import { UserQueryParams } from "../queries/userQuery"
 
 export const serializeUser = (options?: UserQueryParams) => (data: UserData) => {
-  const { id, firstName, lastName, createdAt } = data
+  const { id, firstName, lastName, email, createdAt } = data
   const roles = options?.withRoles ? data.roles!.map((r) => r.name) : undefined
 
-  return { id, firstName, lastName, createdAt, roles }
+  return { id, firstName, lastName, email, createdAt, roles }
 }
