7f716b4fa52a60114be7acabbe956a220e95694c
Enitoni <enitoni@gmail.com>
2021-11-15 16:01:47 +0100

    add option to make authentication required

--- src/modules/auth/middleware/authenticate.ts
+++ src/modules/auth/middleware/authenticate.ts
@@ -1,15 +1,27 @@
 import { Middleware } from "koa"
+import { HttpError } from "../../core/classes/HttpError"
 import { getUser } from "../../user/helpers/getUser"
 import { UserQueryParams } from "../../user/queries/userQuery"
 
-export const authenticate =
-  (options?: UserQueryParams): Middleware =>
-  async (context, next) => {
-    const { user: id } = context.session!
-    if (!id) return next()
+export type Options = {
+  /** The user must be authenticated to use this endpoint */
+  required?: boolean
+}
 
-    const user = await getUser(id, options)
-    context.state.user = user
+export const authenticate =
+  (options?: UserQueryParams & Options): Middleware =>
+  async (context, next) => {
+    const { required, ...rest } = options ?? {}
+    const { user: id } = context.session!
+
+    if (!id && required) {
+      throw new HttpError(401, "Authentication required")
+    }
+
+    if (id) {
+      const user = await getUser(id, rest)
+      context.state.user = user
+    }
 
     return next()
   }
