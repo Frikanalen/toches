14b426318f24fe20ad07baba1bc39fb9b5de7c0e
Enitoni <enitoni@gmail.com>
2021-11-15 18:37:15 +0100

    make sure email is lowercase

--- src/modules/user/schema/userSchema.ts
+++ src/modules/user/schema/userSchema.ts
@@ -1,7 +1,7 @@
 import { InferType, object, string } from "yup"
 
 export const userSchema = object({
-  email: string().email().required().max(255),
+  email: string().email().required().max(255).lowercase(),
   password: string().required().max(255).min(8),
   firstName: string().required().max(255),
   lastName: string().required().max(255),
