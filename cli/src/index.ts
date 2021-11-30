867f63b138b82d5243aaa94a1cb6d070f1f5a05f
Enitoni <enitoni@gmail.com>
2021-11-30 22:23:09 +0100

    Make CLI linkable

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,13 +1,10 @@
+#!/usr/bin/env node
+
 import { program } from "commander"
 import { createMockDataCommand } from "./create-mock-data/command"
 import { testDBConnection } from "./db"
 
 program.addCommand(createMockDataCommand)
-
-program.hook("preAction", async () => {
-  console.info(`Testing DB connection...`)
-  await testDBConnection()
-})
 
 program.hook("postAction", () => process.exit(0))
 program.parse(process.argv)
