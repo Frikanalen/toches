76b3b0ffd22a9c55a4ce8211417dc528929c53f4
Enitoni <enitoni@gmail.com>
2021-12-11 06:36:43 +0100

    make createResource more generic and add 201 status

--- src/modules/core/middleware/createResource.ts
+++ src/modules/core/middleware/createResource.ts
@@ -1,15 +1,13 @@
 import { Middleware, ParameterizedContext } from "koa"
 
-export type CreateAction<D extends object> = (
-  form: any,
-  context: ParameterizedContext,
-) => Promise<D>
+export type CreateAction<D> = (form: any, context: ParameterizedContext) => Promise<D>
 
 export const createResource =
-  <D extends object>(action: CreateAction<D>): Middleware =>
+  <D>(action: CreateAction<D>): Middleware =>
   async (context, next) => {
     const resource = await action(context.state.validated, context)
     context.state.resource = resource
+    context.state.status = 201
 
     return next()
   }
