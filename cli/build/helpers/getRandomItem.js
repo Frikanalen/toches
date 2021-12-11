3d84e005ea3a614cbce07ab99a888728e1bd6fac
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-12-11 14:59:25 +0100

    remove accidentally included build directory

--- cli/build/helpers/getRandomItem.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomItem = void 0;
-const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
-exports.getRandomItem = getRandomItem;
