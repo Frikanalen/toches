af32c74763f590438ae54e8f0eb1ee88b8d78b80
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-12-11 08:05:35 +0100

    encrypt password

--- cli/src/create-test-users/user.ts
+++ cli/src/create-test-users/user.ts
@@ -1,18 +1,21 @@
 import { Command } from "commander"
 import faker from "faker"
 import { db } from "../db"
+import bcrypt from "bcrypt"
 
 export const createAdminCommand = new Command("admin")
   .description("Add admin user to the database")
   .action(async () => {
+    const password = await bcrypt.hash("dev-admin", 12)
     const user = {
         email: "dev-admin@frikanalen.no",
-        password: "dev-admin",
+        password,
         first_name: "Frikanalen admin",
         last_name: "Istrator",
       }
 
-    await db.insert(user).into("users")
+    const [ id ] = await db.insert(user).into("users").returning("id")
+    await db.insert({role_id: 1, user_id: id}).into("role_user_map")
 
     console.info("Generated user. It looks like this:")
     console.info(user)
