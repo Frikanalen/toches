3dac1f8e8078c1e59a4ba81c5b62e64b6f39314d
Enitoni <enitoni@gmail.com>
2021-12-11 07:36:30 +0100

    add jukebox schema

---
+++ migrations/20211211060730_jukebox.js
@@ -0,0 +1,22 @@
+//@ts-check
+
+/**
+ * @param {import("knex").Knex} knex
+ */
+exports.up = async (knex) => {
+  await knex.schema.createTable("jukebox_entries", (table) => {
+    table.increments("id")
+
+    table.integer("video_id").notNullable()
+    table.foreign("video_id").references("videos.id").onDelete("CASCADE")
+
+    table.timestamp("start_at").notNullable()
+  })
+}
+
+/**
+ * @param {import("knex").Knex} knex
+ */
+exports.down = async (knex) => {
+  await knex.schema.dropTable("jukebox_entries")
+}
