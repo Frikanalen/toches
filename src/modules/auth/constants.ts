e0013ce851555fc1a8e8fef6fc82e584dc9d5ad4
Enitoni <enitoni@gmail.com>
2021-12-11 07:10:02 +0100

    warn if FK_API_KEY isn't set

--- src/modules/auth/constants.ts
+++ src/modules/auth/constants.ts
@@ -8,3 +8,9 @@ export const CSRF_SAFE_METHODS = ["GET", "HEAD", "OPTIONS", "TRACE"]
 
 export const SECRET_KEY_HEADER = "X-Api-Key"
 export const SECRET_KEY = process.env.FK_API_KEY
+
+if (!SECRET_KEY) {
+  console.warn(
+    "Warning: FK_API_KEY is not set, this means internal endpoints will be unreachable",
+  )
+}
