6bf3a70666b23aa7ad7defaa4be225592e44620c
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-12-22 18:40:54 +0100

    fix: add missing proxy definition

--- src/modules/proxy/constant.ts
+++ src/modules/proxy/constant.ts
@@ -1,1 +1,2 @@
 export const UPLOAD_RECEIVER_PROXY = "http://localhost:8001"
+export const GRAPHICS_PROXY = "http://localhost:8004"
--- src/modules/proxy/router.ts
+++ src/modules/proxy/router.ts
@@ -1,12 +1,14 @@
 import Router from "@koa/router"
 import { IS_DEV } from "../core/constants"
 import { UPLOAD_RECEIVER_PROXY } from "./constant"
+import { GRAPHICS_PROXY } from "./constant"
 import { createProxy } from "./helper/createProxy"
 
 const router = new Router()
 
 if (IS_DEV) {
   createProxy("/upload/video", UPLOAD_RECEIVER_PROXY, router)
+  createProxy("/graphics", GRAPHICS_PROXY, router)
 }
 
 export { router as proxyRouter }
