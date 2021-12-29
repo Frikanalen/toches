e304db5f4ce79e32eb8094aed77316c73d755547
Enitoni <enitoni@gmail.com>
2021-12-29 21:06:42 +0100

    Fix getJukeboxableVideos returning non broadcastable videos

--- src/modules/scheduling/helpers/getJukeboxableVideos.ts
+++ src/modules/scheduling/helpers/getJukeboxableVideos.ts
@@ -5,6 +5,7 @@ export const getJukeboxableVideos = async () => {
   const [query] = await videoQuery.prepare({})
 
   query.from(videoModel.tableName).where("videos.jukeboxable", true)
+  const videos = await videoModel.parseFromQuery(query)
 
-  return videoModel.parseFromQuery(query)
+  return videos.filter((v) => v.assets.some((a) => a.type === "broadcastable"))
 }
