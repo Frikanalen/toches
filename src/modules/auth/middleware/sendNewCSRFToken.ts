cd5f837eadacddfe0db170df72d2c12a3cca634d
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-06-10 17:18:58 +0200

    mark next as unused with leading underscore

--- src/modules/auth/middleware/sendNewCSRFToken.ts
+++ src/modules/auth/middleware/sendNewCSRFToken.ts
@@ -2,7 +2,7 @@ import { Middleware } from "koa"
 import { CSRF_COOKIE } from "../constants"
 import { createCSRFToken } from "../helpers/createCSRFToken"
 
-export const sendNewCSRFToken = (): Middleware => async (context, next) => {
+export const sendNewCSRFToken = (): Middleware => async (context, _next) => {
   context.cookies.set(CSRF_COOKIE, await createCSRFToken(), {
     httpOnly: false,
   })
