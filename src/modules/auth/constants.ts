bf857b9cda6169e05b2276b8a53d8176e3cbbc91
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-06-10 14:31:20 +0200

    Standardize CSRF cookie name

--- src/modules/auth/constants.ts
+++ src/modules/auth/constants.ts
@@ -3,8 +3,8 @@ import { log } from "../core/log"
 
 export const BCRYPT_COST_FACTOR = 12
 
-export const SESSION_COOKIE = "fk:session"
-export const CSRF_COOKIE = "fk:csrf"
+export const SESSION_COOKIE = "fk-session"
+export const CSRF_COOKIE = "fk-csrf"
 export const CSRF_HEADER = "X-CSRF-Token"
 export const CSRF_SAFE_METHODS = ["GET", "HEAD", "OPTIONS", "TRACE"]
 
