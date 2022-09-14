755059311832dc2cbe2adaac0ee8d96964d586a0
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-09-14 14:56:44 +0200

    Revamp logging and error handling

--- src/modules/core/app.ts
+++ src/modules/core/app.ts
@@ -4,13 +4,13 @@ import { useCSRFProtection } from "../auth/middleware/useCSRFProtection"
 import { useSession } from "../auth/middleware/useSession"
 import { IS_PROD } from "./constants"
 import { handleError } from "./middleware/handleError"
-import { logRequest } from "./middleware/logRequest"
 import { sendCORSHeaders } from "./middleware/sendCORSHeaders"
-
+import logger from "koa-logger"
 import { router } from "./router"
 import { openApiSpec } from "./middleware/sendOpenApiSpec"
 import { apolloServer } from "../graphql/server"
 import { log } from "./log"
+
 const { ui } = require("swagger2-koa")
 
 const app = new Koa()
@@ -18,7 +18,7 @@ const app = new Koa()
 app.proxy = IS_PROD
 
 app.use(ui(openApiSpec, "/swagger"))
-app.use(logRequest())
+app.use(logger())
 app.use(handleError())
 app.use(bodyParser())
 app.use(useSession(app))
--- src/modules/core/middleware/handleError.ts
+++ src/modules/core/middleware/handleError.ts
@@ -1,12 +1,24 @@
 import { Middleware } from "koa"
-import { ValidationError } from "yup"
+import { CSRF_HEADER } from "../../auth/constants"
+import { IS_PROD } from "../constants"
 
+// Small hack to pass along the CORS headers in case of context.throw().
+// If we don't do this, we get network errors rather than the appropriate
+// status code in case of context.throw.
 export const handleError = (): Middleware => async (ctx, next) => {
   try {
     return await next()
-  } catch (error) {
-    if (error instanceof ValidationError) ctx.throw(400, error.errors.toString())
+  } catch (err: any) {
+    if (!IS_PROD) {
+      ctx.set("Access-Control-Allow-Origin", "http://localhost:3000")
+      ctx.set("Access-Control-Allow-Credentials", "true")
+      ctx.set("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE,OPTIONS,HEAD")
+      ctx.set("Access-Control-Allow-Headers", [CSRF_HEADER, "Content-Type"].join(", "))
+    }
 
-    throw error
+    ctx.status = err.statusCode || err.status || 500
+    ctx.body = {
+      message: err.message,
+    }
   }
 }
