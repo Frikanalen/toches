98e233d7607c60c9074ea1f43a93bf04efb01682
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-09-27 01:41:55 +0200

    Beautify resolvers slightly

--- src/modules/graphql/resolvers/resolveOrganization.ts
+++ src/modules/graphql/resolvers/resolveOrganization.ts
@@ -16,11 +16,10 @@ export const resolveOrganizationQuery: Resolver<
   QueryOrganizationArgs
 > = async (parent, args) => {
   const org = await db<Organizations>("organizations")
-    .select("id", "name", "description", "homepage")
-    .select({
+    .select("name", "description", "homepage", {
+      id: db.raw("id::text"),
       createdAt: "created_at",
       updatedAt: "updated_at",
-      orgId: "organization_id",
       brregId: "brreg_number",
       postalAddress: "postal_address",
       streetAddress: "street_address",
@@ -29,13 +28,7 @@ export const resolveOrganizationQuery: Resolver<
     .first()
 
   if (!org) throw new UserInputError(`Organization ${args.id} doesn't exist`)
-
-  const filteredOrg: DeepPartial<Organization> = {
-    ...org,
-    id: org.id.toString(),
-  }
-
-  return filteredOrg
+  return org
 }
 
 export const resolveOrganization: Resolver<
@@ -50,24 +43,22 @@ export const resolveOrganization: Resolver<
 export const resolveOrganizationEditor: Resolver<
   ResolversTypes["OrganizationEditor"],
   DeepPartial<Organization>
-> = async (parent) => {
-  const editor = await db("users")
+> = async (parent) =>
+  await db("users")
     .select("id", "email")
-    .select({ firstName: "first_name", lastName: "last_name" })
+    .select({
+      firstName: "first_name",
+      lastName: "last_name",
+      name: db.raw("first_name || ' ' || last_name"),
+    })
     .where("id", db("organizations").select("editor_id").where("id", parent.id))
     .first()
-
-  return {
-    ...editor,
-    name: `${editor.firstName} ${editor.lastName}`,
-  }
-}
 
 export const resolveOrganizationLatestVideos: Resolver<
   Array<ResolversTypes["Video"]>,
   DeepPartial<Organization>
-> = async (parent) => {
-  return await db("videos")
+> = async (parent) =>
+  await db("videos")
     .select("id")
     .select("description", "media_id", "title", {
       createdAt: "created_at",
@@ -77,4 +68,3 @@ export const resolveOrganizationLatestVideos: Resolver<
     .where("organization_id", parent.id)
     .orderBy("created_at")
     .limit(5)
-}
