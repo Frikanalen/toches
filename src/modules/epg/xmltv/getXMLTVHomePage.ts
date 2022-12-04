5ddfe10eb2d6fc376947ba22e1edb937040ca079
Tore Sinding Bekkedal <toresbe@protonmail.com>
2022-12-04 21:14:31 +0100

    Feh, to hell with it.

--- src/modules/epg/xmltv/getXMLTVHomePage.ts
+++ src/modules/epg/xmltv/getXMLTVHomePage.ts
@@ -2,14 +2,6 @@ import { Middleware } from "koa"
 import { createReadStream } from "fs"
 
 export const getXMLTVHomePage: Middleware = async (context, next) => {
-  // We need the trailing slash for the relative URLs to work
-  if (!context.path.endsWith("/")) {
-    const { request, response } = context
-
-    response.status = 302
-    return response.redirect(request.originalUrl + "/")
-  }
-
   context.type = "text/html"
   context.status = 200
   context.body = createReadStream("src/modules/epg/xmltv/landingPage.html")
