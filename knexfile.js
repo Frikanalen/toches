fae87f28f6c595aa36f78766c2fc7f19f93efbf8
Tore Sinding Bekkedal <toresbe@gmail.com>
2023-07-03 17:44:45 +0200

    Use k8s svc envs in dev mode

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,28 @@
-require("dotenv/config")
+require('dotenv').config({ path: __dirname+'/.env' });
+
+const getConnstring = () => {
+  const {DATABASE_URL} = process.env
+
+  if (process.env.NODE_ENV === "production") {
+    if (!DATABASE_URL?.length) {
+      throw new Error("DATABASE_URL is not set!")
+    }
+
+    return process.env.DATABASE_URL
+  } else {
+    const {POSTGRES_1_0_POSTGRES_SERVICE_HOST, POSTGRES_1_0_POSTGRES_SERVICE_PORT, DB_PASS, DB_USER} = process.env
+
+    if (POSTGRES_1_0_POSTGRES_SERVICE_HOST && POSTGRES_1_0_POSTGRES_SERVICE_PORT) { 
+      return `postgres://${DB_USER}:${DB_PASS}@${POSTGRES_1_0_POSTGRES_SERVICE_HOST}:${POSTGRES_1_0_POSTGRES_SERVICE_PORT}/fk`
+    }
+
+    if(!DATABASE_URL?.length) {
+      throw new Error("DATABASE_URL is not set!")
+    }
+
+    return DATABASE_URL
+  }
+}
 
 module.exports = {
   test: {
@@ -9,13 +33,13 @@ module.exports = {
     },
   },
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connection: getConnstring(),
   migrations: {
     stub: "migration.stub.js",
   },
   postgres: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: getConnstring(),
     migrations: {
       stub: "migration.stub.js",
     },
