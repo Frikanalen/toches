3c9e5d5c79b5c4dff99028a8a1ef6cc838939652
Tore Sinding Bekkedal <toresbe@gmail.com>
2023-07-13 19:54:53 +0200

    Improve documentation

--- src/modules/db/helpers/includeSubquery.ts
+++ src/modules/db/helpers/includeSubquery.ts
@@ -1,9 +1,15 @@
 import { Knex } from "knex"
 import { db } from "../db"
 
-// This is a helper function for Knex that allows you to include a subquery
-// as a column in a query. It's useful for things like including a list of
-// categories or assets in a video query.
+/**
+ *  This is a helper function for Knex that allows you to include a subquery
+ * as a column in a query. It's useful for things like including a list of
+ * categories or assets in a video query.
+ *
+ * @param query Knex query to include
+ * @param queryName Name of the query as it will appear in the result
+ * @returns Query transformed into a subquery
+ */
 export const includeSubquery = (query: Knex.QueryBuilder, queryName: string) =>
   db.raw(`ARRAY(
     SELECT
