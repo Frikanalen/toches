316bed1a67f2e3608c2479243a43f33e74591b56
Tore Sinding Bekkedal <toresbe@gmail.com>
2023-07-27 17:58:28 +0200

    Remove debug cruft

--- src/modules/scheduling/middleware/sendJukeboxableVideos.ts
+++ src/modules/scheduling/middleware/sendJukeboxableVideos.ts
@@ -3,14 +3,7 @@ import { serializeVideo } from "../../video/helpers/serializeVideo"
 import { getJukeboxableVideos } from "../helpers/getJukeboxableVideos"
 
 export const sendJukeboxableVideos = (): Middleware => async (context, next) => {
-  //context.body = (await getJukeboxableVideos()).map((v) => serializeVideo(v))
-
-  context.body = `<html><pre>${JSON.stringify(
-    await getJukeboxableVideos(),
-    null,
-    2,
-  )}</pre></html>`
-  context.status = 200
+  context.body = await getJukeboxableVideos()
 
   return next()
 }
